f429c4d595b6a460aaf0607b51bc0d611d7fb77f
KevinTanDev <kevintan.dev@gmail.com>
2023-03-29 17:59:21 +0200

    Fix home page server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import Home from "@/components/Layouts/Home";
+import { GetServerSideProps } from "next";
 
 export default function Route() {
   return <Home />;
 }
+
+export const getServerSideProps: GetServerSideProps<{}> = async () => {
+  return {
+    props: {},
+  };
+};
+
